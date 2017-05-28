e5172567a61b1936524826741804b0aebf732a16
windowsX <windowsx@windows.com>
2017-05-29 02:23:16 +0530

    first commit

---
+++ src/main/java/com/rajiitg/nginx/controller/SimpleController.java
@@ -0,0 +1,16 @@
+package com.rajiitg.nginx.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("SimpleController")
+@RequestMapping(value = "/", produces="application/json")
+public class SimpleController {
+
+    @RequestMapping(method= RequestMethod.GET, value="/home")
+    public String imHome(Model model){
+        return "Welcome Home!!!";
+    }
+}
