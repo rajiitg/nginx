4e531be6a2cba99a2a5006e84d721a5f599771fb
windowsX <windowsx@windows.com>
2017-05-31 01:43:15 +0530

    first commit

--- src/main/java/com/rajiitg/nginx/SimpleApplication.java
+++ src/main/java/com/rajiitg/nginx/SimpleApplication.java
@@ -1,12 +1,17 @@
 package com.rajiitg.nginx;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class SimpleApplication {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(SimpleApplication.class, args);
+        FileUtils.touch(new File("/tmp/app-initialized"));
     }
 }
